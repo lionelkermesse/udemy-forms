706d4b95138eed74c5c32ed35f14fd535f712dd2
Lionel KEMINSE <lionelkermesse@gmail.com>
2023-10-11 16:12:47 +0200

    implement a user form using template form approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('signupForm', {static: false}) signupForm: NgForm;
+  defaultQuestion: string ='pet';
+  defaultAnswer: string = '';
+  genders: string[] = ['male', 'female'];
+  submitted: boolean;
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  }
+
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('form submitted', form);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset();
   }
 }
